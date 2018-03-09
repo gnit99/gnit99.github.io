a86178aeb9a51470bc39ab0d837d5dbfb4cf1bc5
gnit99 <36211393+gnit99@users.noreply.github.com>
2018-03-09 13:24:17 +0200

    Guta 1.11

--- JS/script.js
+++ JS/script.js
@@ -2,7 +2,7 @@ var clock = document.getElementById("clock");
 
 function hexoClock() {
 	var time = new Date();
-	var h =(time.getHours() % 12).toString();
+	var h =(time.getHours() % 24).toString();
 	var m =time.getMinutes().toString();
 	var s =time.getSeconds().toString();
 	
@@ -18,7 +18,7 @@ function hexoClock() {
 	var clockString = h + ":" + m +":"+ s;
 	 
 	clock.textContent = clockString;
-	document = clockString
+	document = clockString;
 }
 
 setInterval(hexoClock, 1000);
